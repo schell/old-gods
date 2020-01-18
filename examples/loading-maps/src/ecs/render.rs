75291e0d51298128a92b0026fe5eaaab6f3b0080
Schell Scivally <efsubenovex@gmail.com>
2020-01-18 10:40:07 -0800

    implemented resources for htmlresources struct #20

--- examples/loading-maps/src/ecs/render.rs
+++ examples/loading-maps/src/ecs/render.rs
@@ -1,41 +1,153 @@
 use old_gods::prelude::*;
+use std::{
+  collections::HashMap,
+  sync::{
+    Arc,
+    Mutex
+  },
+};
 use wasm_bindgen::prelude::*;
-use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
+use wasm_bindgen::{
+  JsCast,
+  UnwrapThrowExt
+};
+use web_sys::{
+  window,
+  CanvasRenderingContext2d,
+  ErrorEvent,
+  EventTarget,
+  HtmlImageElement,
+  Window,
+};
+
+
+#[derive(Clone)]
+pub enum LoadStatus {
+  None,
+  Started ,
+  Complete,
+  Error(String)
+}
 
 
 trait Resources {
   type Texture;
 
-  fn has_sprite_sheet<S:Into<String>>(&self, s:S) -> bool;
+  fn status_sprite_sheet<S:Into<String>>(&self, s:S) -> LoadStatus;
   fn load_sprite_sheet<S:Into<String>>(&mut self, s:S);
-  fn take_sprite_sheet<S:Into<String>>(&mut self, s:S) -> Option<Self::Texture>;
+  fn take_sprite_sheet(&mut self, s:&str) -> Option<Self::Texture>;
   fn put_sprite_sheet<S:Into<String>>(&mut self, s:S, tex:Self::Texture);
 }
 
 
+pub struct Callbacks(Arc<Closure<dyn Fn(JsValue)>>, Arc<Closure<dyn Fn(JsValue)>>);
+
+
 pub struct HtmlResources {
+  sprite_sheets: HashMap<String, Arc<Mutex<(LoadStatus, Option<HtmlImageElement>)>>>,
+  callbacks: HashMap<String, Callbacks>
 }
 
-// TODO: Implement Resources for HtmlResources struct
+
+impl HtmlResources {
+  pub fn new() -> Self {
+    HtmlResources {
+      sprite_sheets: HashMap::new(),
+      callbacks: HashMap::new()
+    }
+  }
+}
+
+
+// TODO: Test Resources for HtmlResources implementation
 impl Resources for HtmlResources {
   type Texture = HtmlImageElement;
 
-  fn has_sprite_sheet<S:Into<String>>(&self, s:S) -> bool {
-    false
+  fn status_sprite_sheet<S:Into<String>>(&self, s:S) -> LoadStatus {
+    self
+      .sprite_sheets
+      .get(&s.into())
+      .map(|payload| {
+        let status_and_may_img =
+          payload
+          .try_lock()
+          .unwrap();
+        status_and_may_img.0.clone()
+      })
+      .unwrap_or(LoadStatus::None)
   }
 
   fn load_sprite_sheet<S:Into<String>>(&mut self, s:S) {
-
+    let path = s.into();
+    let img =
+      window().unwrap_throw()
+      .document().unwrap_throw()
+      .create_element("img").unwrap_throw()
+      .dyn_into::<HtmlImageElement>().unwrap_throw();
+    img.set_src(&path);
+    let status =
+      Arc::new(Mutex::new((
+        LoadStatus::Started,
+        img.clone(),
+      )));
+    let target:&EventTarget =
+      img
+      .dyn_ref()
+      .unwrap_throw();
+    let load_status = status.clone();
+    let load =
+      Closure::wrap(Box::new(move |_:JsValue| {
+        let mut status_and_img =
+          load_status
+          .try_lock()
+          .unwrap_throw();
+        status_and_img.0 = LoadStatus::Complete;
+      }) as Box<dyn Fn(JsValue)>);
+    let err_status = status.clone();
+    let err =
+      Closure::wrap(Box::new(move |event:JsValue| {
+        let mut status_and_img =
+          err_status
+          .try_lock()
+          .unwrap_throw();
+        let event =
+          event
+          .dyn_into::<ErrorEvent>()
+          .unwrap_throw();
+        let msg =
+          event
+          .error()
+          .as_string()
+          .unwrap_or("unknown error".into());
+        status_and_img.0 = LoadStatus::Error(msg);
+      }) as Box<dyn Fn(JsValue)>);
+    target
+      .add_event_listener_with_callback("load", load.as_ref().unchecked_ref())
+      .unwrap();
+    target
+      .add_event_listener_with_callback("error", err.as_ref().unchecked_ref())
+      .unwrap();
+    self
+      .callbacks
+      .insert(path, Callbacks (Arc::new(load), Arc::new(err)));
   }
 
-  fn take_sprite_sheet<S:Into<String>>(&mut self, s:S) -> Option<Self::Texture> {
-    panic!("")
+  fn take_sprite_sheet(&mut self, s:&str) -> Option<Self::Texture> {
+    let _ = self.callbacks.remove(s)?;
+    let status_and_img = self.sprite_sheets.remove(s)?;
+    let status_and_img = status_and_img.try_lock().ok()?;
+    status_and_img.1.clone()
   }
 
   fn put_sprite_sheet<S:Into<String>>(&mut self, s:S, tex:Self::Texture) {
-
+    let status_and_img =
+      Arc::new(Mutex::new((LoadStatus::Complete, Some(tex))));
+    self
+      .sprite_sheets
+      .insert(s.into(), status_and_img);
   }
 }
+
 
 /// Draw a sprite frame at a position.
 pub fn draw_sprite(
@@ -95,14 +207,23 @@ pub fn draw_rendering(
 ) {
   match &r.primitive {
     RenderingPrimitive::TextureFrame(f) => {
-      let has_texture =
+      let texture_status =
         resources
-        .has_sprite_sheet(&f.sprite_sheet);
-      if !has_texture {
-        resources
-          .load_sprite_sheet(f.sprite_sheet.as_str());
-        // Come back later because it's loading etc.
-        return;
+        .status_sprite_sheet(&f.sprite_sheet);
+      match texture_status {
+        LoadStatus::None => {
+          // Load it and come back later
+          resources
+            .load_sprite_sheet(f.sprite_sheet.as_str());
+        }
+        LoadStatus::Started => {
+          // Come back later because it's loading etc.
+          return;
+        }
+        LoadStatus::Complete => {}
+        LoadStatus::Error(msg) => {
+          warn!("sprite sheet loading error: {}", msg);
+        }
       }
 
       let tex =
